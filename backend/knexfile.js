e1f18676b851d67ad062659450e69b6e87131067
quynghienroi <nguyenlehongquy@gmail.com>
2026-06-29 17:25:39 +0700

    fix: add production config to knexfile for render

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -14,5 +14,18 @@ module.exports = {
     seeds: {
       directory: path.join(__dirname, 'database', 'seeds')
     }
+  },
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, 'database', 'diaplus.db')
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.join(__dirname, 'database', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'database', 'seeds')
+    }
   }
 };
